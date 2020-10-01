298ddab80453489ccb1ad7dfb8264e9875ce9be1
mvenh_000 <mvenh_000@Lenovo>
2020-10-01 21:56:21 +0200

    second commit

---
+++ DatabaseConnection/src/Connect.java
@@ -0,0 +1,28 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class Connect {
+	static Connection connection = null;
+	static String databaseName = "Database Connecion";
+	static String url = "jdbc:mysql://localhost:3306/" +databaseName;
+	static String username = "root";
+	static String password = "21may2001";
+	
+
+	public static void main(String[] args) throws InstantiationException, IllegalAccessException, ClassNotFoundException, SQLException {
+		Class.forName("com.mysql.jdbc.Driver").newInstance();
+		connection = DriverManager.getConnection(url, username, password);
+		PreparedStatement ps = connection.prepareStatement("INSERT INTO 'studentdatabase'.'student' (name) VALUES ('mvenhle shikovha);");
+		int status = ps.executeUpdate();
+		
+		if (status !=0) {
+			System.out.println("Database connected successfully");
+			
+		}
+	
+
+	}
+
+}
--- Sem3Lab1/src/Mysqltable.java
+++ Sem3Lab1/src/Mysqltable.java
@@ -4,7 +4,7 @@ public static void main(String args[]){
 try{  
 Class.forName("com.mysql.jdbc.Driver");  
 Connection con=DriverManager.getConnection(  
-"jdbc:mysql://localhost:3306/sonoo","root","21may2001");  
+"jdbc:mysql://localhost:3306/java connection","root","21may2001");  
 //here sonoo is database name, root is username and password  
 Statement stmt=con.createStatement();  
 ResultSet rs=stmt.executeQuery("select * from emp");  
